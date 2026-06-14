6cf25b99e5690590bc0cd6c3379d88e18af48501
Dexter Yoon <dexteryoon@Dexterui-MacBookPro.local>
2026-06-14 17:02:49 +0900

    database update

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,8 @@
 import { z } from "zod";
+
+export const mobileValidator = (mobile: string) => {
+  if (!mobile) return "";
+  const { data, success, error } = z.number().safeParse(mobile);
+
+  if (!mobile.startsWith("010")) return "";
+};
